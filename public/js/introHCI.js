2f7d4c3d224eace174d7acfca82839db0554ad9a
vagrant <karen.liyu.wu@gmail.com>
2014-02-14 21:43:07 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,8 +26,32 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/"+idNumber, callbackFunction);
+	console.log("URL " + "/project/"+ idNumber);
+
+	//($("#"+projectID).find('.details')).html('oogabooga');
+	//($("#"+projectID).find('.details')).html(projectHTML);
+
+	function callbackFunction(result)
+	{
+		console.log("from call back " + result);
+
+		var projectHTML ='<a href="#" class="thumbnail>"'+
+		'<img src="'+ result['image'] + '" class="detailsImage">' +
+		'<p>' + result['title'] + '</p>' +
+		'<p><small>' + result['date'] + '<br>' + result['summary'] +'</small></p></a>';
+
+	//($(this.find('.details')).html(projectHTML);
+	($("#"+projectID).find('.details')).html(projectHTML);
+
+
+	}
+
 }
+
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
@@ -35,4 +59,16 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	$.get("/palette", callbackFunction);
+
+	function callbackFunction(result)
+	{
+		var colors = result['colors']['hex'];
+		$('body').css('background-color', colors[0]);
+		$('.thumbnail').css('background-color', colors[1]);
+		$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+		$('p').css('color', colors[3]);
+		$('.project img').css('opacity', .75);
+	}
 }
